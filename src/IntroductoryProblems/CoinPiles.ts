1cae5031c045b1c3d8fb062a064ee6653b571c7f
Yury <72691412+cheatsnake@users.noreply.github.com>
2021-11-30 19:21:54 +0300

    Coin Piles

---
+++ src/IntroductoryProblems/CoinPiles.ts
@@ -0,0 +1,10 @@
+import read from "../index";
+
+function coinPiles(a: number, b: number): "YES" | "NO" {
+    return (a + b) % 3 === 0 && 2 * b >= a && 2 * a >= b ? "YES" : "NO";
+}
+
+read.on("line", function (line: string) {
+    const vals: number[] = line.split(" ").map(Number);
+    console.log(coinPiles(vals[0], vals[1]));
+});
