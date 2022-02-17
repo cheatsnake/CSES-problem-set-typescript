1bca21fda3ca6e77911456c1c3d3dfb3355340a8
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-02-17 19:22:52 +0300

    Update GrayCode

--- src/IntroductoryProblems/GrayCode.ts
+++ src/IntroductoryProblems/GrayCode.ts
@@ -1,13 +1,17 @@
 import read from "../index";
 
+// Solution ------------------------
 function grayCode(n: number): void {
     const max = Math.pow(2, n);
     for (let i = 0; i < max; i++) {
         console.log(i.toString(2).padStart(n, "0"));
     }
 }
+// --------------------------------------
 
 read.on("line", function (line: string) {
     const int = Number(line);
     grayCode(int);
 });
+
+export default grayCode;
