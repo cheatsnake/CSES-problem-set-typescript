f03a4fc6d53c6266fc73735c8617614ebeee55d3
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-02-17 19:22:12 +0300

    Update BitStrings

--- src/IntroductoryProblems/BitStrings.ts
+++ src/IntroductoryProblems/BitStrings.ts
@@ -1,9 +1,12 @@
 import read from "../index";
 
-const bitString = (bitLen: number): number => 
-    Math.pow(2, bitLen);
+// Solution ------------------------------------------------------
+const bitString = (bitLen: number): number => Math.pow(2, bitLen);
+// --------------------------------------
 
 read.on("line", function (line: string) {
     const int = Number(line);
     console.log(bitString(int));
 });
+
+export default bitString;
