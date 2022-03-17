e2f4417ab7b425fdf905dc551be3dcdba5a5340d
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-03-17 22:44:01 +0300

    Update Number Spiral

--- src/IntroductoryProblems/NumberSpiral.ts
+++ src/IntroductoryProblems/NumberSpiral.ts
@@ -1,18 +1,30 @@
-import read from "../index";
+import { rl, readConsole } from "../index";
 
+// Solution ---------------------------------
 function numberSpiral(y: number, x: number) {
     if (y > x) {
-        return y % 2 === 0 
-            ? y * y - x + 1 
-            : y * y - (y + y - 1) + x;
+        return y % 2 === 0 ? y * y - x + 1 : y * y - (y + y - 1) + x;
     } else {
-        return x % 2 == 0 
-            ? x * x - (x + x - 1) + y 
-            : x * x - y + 1;
+        return x % 2 == 0 ? x * x - (x + x - 1) + y : x * x - y + 1;
     }
 }
+// ------------------------------------------
 
-read.on("line", function (line: string) {
-    const vals = line.split(" ");
-    console.log(numberSpiral(+vals[0], +vals[1]));
-});
+const main = async () => {
+    const count = Number(await readConsole());
+    const lines: string[] = [];
+
+    for (let i = 0; i < count; i++) {
+        lines.push(await readConsole());
+    }
+    rl.close();
+
+    lines.forEach((line) => {
+        const values = line.split(" ").map(Number);
+        console.log(numberSpiral(values[0], values[1]));
+    });
+};
+
+main();
+
+export default numberSpiral;
