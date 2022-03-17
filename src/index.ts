169b984dcbafbaa9cd96b23570409727856c8260
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-03-17 22:41:07 +0300

    Environment updates for reading the console

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import readline from "readline";
 
-const read = readline.createInterface({
+export const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     terminal: false,
 });
 
-export default read;
+export const readConsole = (): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        try {
+            rl.question("", (result) => resolve(result));
+        } catch {
+            reject("No data");
+        }
+    });
+};
