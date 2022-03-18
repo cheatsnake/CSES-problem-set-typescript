5b67f1ce376c7977ec8eff704573895ffb1687f2
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-03-18 07:21:37 +0300

    Update Towers

--- src/SortingAndSearching/Towers.ts
+++ src/SortingAndSearching/Towers.ts
@@ -1,22 +1,37 @@
-import read from "../index";
+import { rl, readConsole } from "../index";
 
+// Solution ----------------------------
 function towers(arr: number[]): number {
     const results: number[] = [];
 
-    arr.forEach(elem => {
+    arr.forEach((elem) => {
         const position = results
             .sort((a, b) => a - b)
-            .findIndex(item => item > elem);
+            .findIndex((item) => item > elem);
 
-        position === -1 
-            ? results.push(elem)
-            : results[position] = elem
-    })
+        position === -1 ? results.push(elem) : (results[position] = elem);
+    });
 
-    return results.length
+    return results.length;
 }
+// ----------------------
 
-read.on("line", function (line: string) {
-    const vals: number[] = line.split(" ").map(Number);
-    console.log(towers(vals));
-});
+const main = async () => {
+    let count = 2;
+    const lines: string[] = [];
+
+    for (let i = 0; i < count; i++) {
+        lines.push(await readConsole());
+    }
+    rl.close();
+
+    const values = lines[1]
+        .split(" ")
+        .slice(0, +lines[0])
+        .map(Number);
+    console.log(towers(values));
+};
+
+main();
+
+export default towers;
