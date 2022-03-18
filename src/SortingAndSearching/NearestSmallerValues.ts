64d11a697dd10db227947d98b0ccc64fb84aac4c
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-03-18 07:20:07 +0300

    Update Nearest Smaller Values

--- src/SortingAndSearching/NearestSmallerValues.ts
+++ src/SortingAndSearching/NearestSmallerValues.ts
@@ -1,6 +1,6 @@
-import read from "../index";
+import { rl, readConsole } from "../index";
 
-// Solution ------------------------------------------
+// Solution ----------------------------------------
 function nearestSmallerValues(n: number[]): string {
     const results: number[] = [];
 
@@ -18,9 +18,24 @@ function nearestSmallerValues(n: number[]): string {
 
     return results.join(" ");
 }
-// --------------------------------------
+// -------------------------
 
-read.on("line", function (line: string) {
-    const vals: number[] = line.split(" ").map(Number);
-    console.log(nearestSmallerValues(vals));
-});
+const main = async () => {
+    let count = 2;
+    const lines: string[] = [];
+
+    for (let i = 0; i < count; i++) {
+        lines.push(await readConsole());
+    }
+    rl.close();
+
+    const values = lines[1]
+        .split(" ")
+        .slice(0, +lines[0])
+        .map(Number);
+    console.log(nearestSmallerValues(values));
+};
+
+main();
+
+export default nearestSmallerValues;
