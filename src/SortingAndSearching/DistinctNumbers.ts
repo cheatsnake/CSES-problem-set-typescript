bf94fb0b72e80314ac387d0b4ac4c36e61dfb98b
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-03-18 07:18:46 +0300

    Update Distinct Numbers

--- src/SortingAndSearching/DistinctNumbers.ts
+++ src/SortingAndSearching/DistinctNumbers.ts
@@ -1,5 +1,6 @@
-import read from "../index";
+import { rl, readConsole } from "../index";
 
+// Solution ----------------------------------------
 function distinctNumbers(values: number[]): number {
     const result: number[] = [];
     values.forEach((elem) => {
@@ -7,8 +8,24 @@ function distinctNumbers(values: number[]): number {
     });
     return result.length;
 }
+// ----------------------
 
-read.on("line", function (line: string) {
-    const vals: number[] = line.split(" ").map(Number);
-    console.log(distinctNumbers(vals));
-});
+const main = async () => {
+    let count = 2;
+    const lines: string[] = [];
+
+    for (let i = 0; i < count; i++) {
+        lines.push(await readConsole());
+    }
+    rl.close();
+
+    const values = lines[1]
+        .split(" ")
+        .slice(0, +lines[0])
+        .map(Number);
+    console.log(distinctNumbers(values));
+};
+
+main();
+
+export default distinctNumbers;
